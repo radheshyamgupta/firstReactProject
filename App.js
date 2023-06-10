767f2b74308fcebed5be23e9a86901685acaab6d
Radheshyam gupta <130499661+radheshyamgupta@users.noreply.github.com>
2023-06-11 05:21:16 +0530

    firstReactProject

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import ExpenseItem from './Component/ExpenseItem';
+function App() {
+  return (
+    <div >
+      <header >
+       <ExpenseItem></ExpenseItem>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h2>Expense Item</h2>
+            <header>
+                <span><h2>Food:10</h2></span><span><h2>Petrol:100</h2></span><span> <h2>Movie:200</h2></span>
+            </header>
+        </div>
+    )
+}
+export default ExpenseItem;
