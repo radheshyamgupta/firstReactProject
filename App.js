9b74aad904527abdc4bd8050f72316dcf8d50cb9
Radheshyam gupta <130499661+radheshyamgupta@users.noreply.github.com>
2023-06-11 14:40:18 +0530

    added few items to the expense list

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 
 import ExpenseItem from './Component/ExpenseItem';
 function App() {
+ const  expense=[
+  {
+  title:"Car insurance",
+  price:250,
+  date:new Date(2011,2,1),
+  location1:"mumbai"
+ },
+ {
+  title:"Bike insurance",
+  price:250,
+  date:new Date(2011,2,1),
+  location1:"kandivali"
+ }
+]
+
   return (
     <div >
-      <header >
-       <ExpenseItem></ExpenseItem>
-      </header>
+    <ExpenseItem title={expense[0].title} price={expense[0].price} location1={expense[0].location1} date={expense[0].date} ></ExpenseItem>
+    <ExpenseItem title={expense[1].title} price={expense[1].price} location1={expense[1].location1} date={expense[1].date} ></ExpenseItem>
     </div>
   );
 }
--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,10 +1,14 @@
-function ExpenseItem(){
+import './ExpenseItem.css'
+function ExpenseItem(props){
+
+  
     return (
-        <div>
-            <h2>Expense Item</h2>
-            <header>
-                <span><h2>Food:10</h2></span><span><h2>Petrol:100</h2></span><span> <h2>Movie:200</h2></span>
-            </header>
+        <div className="expense-item">
+            <h2 >Expense Item</h2>
+                <div className='expenseName'> expenseName:{props.title}</div>
+                <div className='date'>Date:{props.date.toDateString()}</div>
+                <div className='expensePrice'>expensePrice:${props.price}</div>
+                <div className='LocationOfExpenditure'>expenseLocation:{props.location1}</div>
         </div>
     )
 }
