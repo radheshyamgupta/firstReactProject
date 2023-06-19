507c8a2b38a9856d987b4882b7cc65f515b6f60c
Radheshyam gupta <130499661+radheshyamgupta@users.noreply.github.com>
2023-06-19 08:56:32 +0530

    submitted the forms  and data stored in obj

--- Form.js
+++ Form.js
@@ -1,28 +1,55 @@
 import "./Form.css"
+import   React , { useState} from 'react'
+
 function Form(){
-    function title (e){
+    const [inputTitle,setTitle] = useState('')
+const [inputPrice,setPrice]=useState('')
+const [inputLocation,setLocation] =useState('')
+const [inputDate,setDate]= useState('')
+
+ function submitHandler(e){
+    e.preventDefault()
+ const submitData={
+    title:inputTitle,
+    price:inputPrice,
+    location:inputLocation,
+    date:inputDate
+
+}
+console.log(submitData)
+}
+const date=(e)=>{
+    e.preventDefault()
+    setDate(e.target.value)
+}
+
+    const title = (e)=>{
 e.preventDefault()
-console.log(e.target.value)
+setTitle(e.target.value)
 
     }
-    function price(e){
-console.log(e.target.value)
+    const price=(e)=>{
+        setPrice(e.target.value)
     }
-    function location(e){
-console.log(e.target.value)
+    const location=(e)=>{
+        setLocation(e.target.value)
     }
 
     return(
     
 <div className="Form">
-    
+    <form  onSubmit={submitHandler}>
     <label for="title">Title</label>
     <input type="text" id="input" onChange={title}></input>
     <label for="number">Price</label>
     <input type="number" id="price"  onChange={price}></input>
     <label for="location" id="location"> Location</label>
     <input type="text" onChange={location}></input>
-    <button  >ADD</button>
+    <lable for="date" >Date</lable>
+    <input type="date" onChange={date}></input>
+    <button type="submit" >ADD</button>
+    </form>
+    
 </div>
     )
 }
